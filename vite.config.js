56f1b98e94b796e9b12e0bf88f170ddbe8a6a65a
Abhilash A <abhilashanil40404@gmail.com>
2024-08-17 15:24:12 +0530

    updated vite configs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/machine-coding-2/',
   plugins: [react()],
   envPrefix: "CAC_",
 });
