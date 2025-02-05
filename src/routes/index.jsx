6c5f9a45db5d0afffb9bfb93a20d792490ce4ac6
AbhiImReal403 <116947436+AbhiImReal403@users.noreply.github.com>
2025-02-06 01:52:09 +0530

    Update index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,34 +3,34 @@ import { RandomUser, RandomJokes, CatsListing, PageNotFound } from "../pages";
 import { GodMode, PublicLayout } from "../layouts";
 
 export default [
-  {
-    path: "/gods-mode",
-    Component: GodMode,
-    errorElement: PageNotFound,
-    children: [
-      {
-        index: true,
-        Component: RandomUser,
-      },
-      {
-        path: "random-user",
-        Component: RandomUser,
-      },
-      {
-        path: "random-jokes",
-        Component: RandomJokes,
-      },
-      {
-        path: "cats-listing",
-        Component: CatsListing,
-      },
+  // {
+  //   path: "/gods-mode",
+  //   Component: GodMode,
+  //   errorElement: PageNotFound,
+  //   children: [
+  //     {
+  //       index: true,
+  //       Component: RandomUser,
+  //     },
+  //     {
+  //       path: "random-user",
+  //       Component: RandomUser,
+  //     },
+  //     {
+  //       path: "random-jokes",
+  //       Component: RandomJokes,
+  //     },
+  //     {
+  //       path: "cats-listing",
+  //       Component: CatsListing,
+  //     },
 
-      {
-        path: "*",
-        Component: PageNotFound,
-      },
-    ],
-  },
+  //     {
+  //       path: "*",
+  //       Component: PageNotFound,
+  //     },
+  //   ],
+  // },
   {
     path: "/",
     Component: PublicLayout,
